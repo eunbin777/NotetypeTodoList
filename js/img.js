60da6e8b3c60b3cc286b1abbafb0deeccd5952c4
eunbin777 <103042314+eunbin777@users.noreply.github.com>
2022-05-24 04:53:52 +0900

    solve error

--- js/img.js
+++ js/img.js
@@ -1,7 +1,8 @@
 const img = document.querySelector(".image .content");
 const imageList = ["0.jpg","1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg"];
+console.log(imageList);
 console.log(img);
 
 const chosenImage = imageList[Math.floor(Math.random() * imageList.length)];
-
-img.style.backgroundImage = `url(image/${chosenImage})`;
+console.log(chosenImage);
+img.style.backgroundImage = `url(img/${chosenImage})`;
